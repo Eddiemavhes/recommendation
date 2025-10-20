2e05d1619b88bff3d268fe8ae42488bbb6262d88
Eddiemavhes <edmavhe@gmail.com>
2025-10-20 15:14:10 +0200

    Update settings.py

--- job_matcher/settings.py
+++ job_matcher/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g1oi5=@0ggna$t=k9pd##xe+#v5z#dyk2odv$o-y=k4#-b@km8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # URL Configuration
 APPEND_SLASH = True
