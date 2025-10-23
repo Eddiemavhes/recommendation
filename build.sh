52488fda986671d32351a206eb237b0f2efaf82d
Your Name <your.email@example.com>
2025-10-23 20:58:55 +0200

    Add debugging to build script and configure WhiteNoise

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Python version:"
+python --version
+
+echo "Current working directory:"
+pwd
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Database URL (censored):"
+echo ${DATABASE_URL:0:20}...
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Running database migrations..."
+python manage.py showmigrations
+python manage.py migrate --plan
+python manage.py migrate
